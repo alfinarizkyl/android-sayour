2c56160377694fc9065b6907385f74aab7e5ee3c
alfinarizkyl <alfinarizkyl@gmail.com>
2021-06-03 18:51:32 +0700

    Sayour

---
+++ app/src/main/java/id/myeco/sayour/profil.kt
@@ -0,0 +1,11 @@
+package id.myeco.sayour
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class profil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
